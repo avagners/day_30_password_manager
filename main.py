685d8062960d73b159ac54f4dc3387bae0dbcdae
Alexander Vagner <onemybox@gmail.com>
2022-02-08 16:10:52 +0300

    add json format for data; add try/except block

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -31,18 +32,29 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            'email': email,
+            'password': password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title='Oops', message="Please make sure haven't left any field empty.")
     else:
-        is_ok = messagebox.askokcancel(
-            title=website, message=f'There are details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?'
-        )
-        if is_ok:
-            with open('data.txt', 'a') as data_file:
-                data_file.write(f'{website} | {email} | {password}\n')
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open('data.json', 'r') as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open('data.json', 'w') as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -69,7 +81,7 @@ website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=43)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, 'onemybox@gmail.com')
+email_entry.insert(0, 'your_email@mail.com')
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
